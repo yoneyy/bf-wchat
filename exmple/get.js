5f057586308fb1975bf8a36d3c21aba015c4c064
root <yoneyy>
2022-02-09 11:40:36 +0800

    feat: Edit exmple get wework/welink chat datas

--- exmple/get.js
+++ exmple/get.js
@@ -1,41 +1,50 @@
-const We = require('../dist/index').default;
+const BFWchat = require('../dist/index').default;
 
+// your RSA private key
 const private_key = `
 -----BEGIN RSA PRIVATE KEY-----
-MIICXgIBAAKBgQC6QebGUVX1UFvnAIHNsn374gZCl78wqOVf8irGNBokNfL4STs7
-LsswdFQy4sOk7LjObEEOntoyemHIoa1TGQs9rSsgSAoRuX+b3afyuRRvSL+J7eAf
-uuIBPQQUh24FjAmCceJ34HKqRcVEOga3AX41LASTXCscfQfiuSiUteVMqQIDAQAB
-AoGADB+Tf5eWNn45SScri8HBOQasfSOm7moj6fOXjKiiBfbb/oS3vgGs+JWt0FUZ
-wm+lFMyPjH3S+oOkPzykzTqdVFT78XJpFYpyAIln3FMBPNvTT/OMq6XDhYUIxHP9
-I3a1aaw88sjh45whdLNoU6EE/pgQ7Sxdk21K6/ZlcvRb50ECQQD38wJuXyOuWYFv
-cU2cUmljl0D1ldTsTC3Rd3hgMs237YoRcxAx+Rloxzub25LbdcU/AWQe12eFyadn
-Y3V5ZG1JAkEAwE4Zt9k7lBSn05D+3qQOBeoFIjOvf1+DHphywkyUZIH4IKoJRsMc
-tQUQtnjbD+VSpqGPJxPGjrWv4+/M1czEYQJBAKtyOQggeQehlEJ+0e95fK4EdEJc
-LDKIKlZgibfa6Sx477NJILWtTMiKmskWduGQ+aarS5OX1llIKpzuxNf/fSECQQCS
-+R3ezUAjpMW/hInGT0wIaRsTUL1IdA8aSxkqNq0TQ+IH1OzSEDzYfM6RJz0FoRbS
-E5f9HbgbSfDsHFJI9P8BAkEAjVBRbmRxiWBF7DCyFQtJ4I1e4zcOiZFIAuFWRfoV
-4OEzK42SKE9GBnglkZJ7+LmVDD6UivF2Rs+Eu80j+gsl7w==
+MIIEpAIBAAKCAQEAvsFopMih+xeS6v1FMIM4i0g4KAByrVGVFWaK4gWW+tzyAslh
+EfAvhcUURjYuF3M5OhoLEiOnK+Am7DzQzynq641PAl70912vR7/RRNPwwuOQR1g4
+mOnX4Y2xkrh+nN3zEwVPDS5g32XG4Qlo+T2+6psLuCpaBM+B+YDzeOOxlygsb3Pb
+3cT9o57yZEZIs3AZDEQLZrGhO0Y1TJRaaHWLPJxGrtc3FhBibxbOkIaY3Gl5zAJv
+aVorSJeauMIjbSD0n+zFXo92YcLmuchRtQJCfumOD1jvgXkK3cvKb5TWyeoSSx18
+5uaNXmymH/4AymP+rY0rg+H6YjHE7z8cjMj3DQIDAQABAoIBABXS9u/zbGTX2507
+ANF+EdgNmS8qbfRaYx4hrz4QuadV4/Jhq9NrkWRSmfE2fat9KUz2cdHPdm7UpF9e
+zd0FxLTSY/m3d3tdopIo5f5j6SXC91zSQRdtmZ9aoLhSl1nHIupILzUo2zhz4n8Y
+noP1hoQ83OhjIA1w2EP9dt2b31OVuCmXPyAtUZwSNtcef6/LFyiFC2p+13ZgfTyj
+BTlc2hlVqYZCafZbxYGHUFIpEH2B9EHHphW2iXf7kNLBxKKfm0AEUfJIIsI9ktUa
+Z7DC1+NA773aeM6Q/D4R0wKBgQCB732GK/Qk9upOFrtqDHr/bpHcn5GnPZgyUTgv
+O5trbwIfd57Yy9nY0knEa1W90Du57Mamn+c2NplhKKTnh1jDVsm8op7IeUy+PpJ4
+u4J8Igr9xGju6QQO8+lCyx9HxTREHpq7eIUdEb7Z6o3w4mwr/CBTg9j5MvZUgqib
+8ZC1RwKBgQDOyxG7BF47AO0UeJqobBoruvWzG8Aa8dAlifAm3IWglljWxbU2xZYI
+tXlupHg1TZkWWQU3M3GPySCZCkJJdxLPyMdx/qPzlfr4TU2eJ0secxDgnp+n+L+Y
+fOFkJ6nhtLm61i3vgrX8yQh+sVzv8jzqzpYGUbz7AjcTInYF2h0N/Q==
 -----END RSA PRIVATE KEY-----
 `;
 
-const we = new We({
-  corpid: 'ww4c808edd7916ad78',
-  secret: 'mo7lza3YYFPXFPJRHLJpifkmYth8lwLNdH3eOoaAhV8',
+// Initialize WeWork Chat SDK
+const bf_wchat = new BFWchat({
+  corpid: 'your corpid of Wework',
+  secret: 'your secret of Wework',
   private_key
 });
 
 async function main() {
-  const chat_list = await we.getChatData({
+  
+  // Get chat data list
+  const chat_list = await bf_wchat.getChatData({
     seq: 839,
     limit: 2,
     timeout: 5
   });
   console.log(chat_list);
 
-  const encrypt_key = await we.deRSAcryptChatData(chat_list['chatdata'][1]['encrypt_random_key']);
+  // Obtain the encrypt_key through RSA decryption
+  const encrypt_key = await bf_wchat.deRSAcryptChatData(chat_list['chatdata'][1]['encrypt_random_key']);
   console.log(encrypt_key);
 
-  const content = await we.deCryptDatas(encrypt_key, chat_list['chatdata'][1]['encrypt_chat_msg']);
+  // Decrypt the chat content by the obtained encrypt_key
+  const content = await bf_wchat.deCryptDatas(encrypt_key, chat_list['chatdata'][1]['encrypt_chat_msg']);
   console.log(content);
 }
 
